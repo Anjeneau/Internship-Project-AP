60f8f62ad0ce12082e380892ed4e422bc272f1f5
Anjeneau <AnjeneauPro@gmail.com>
2025-04-23 00:25:52 -0400

    April Internship Project

---
+++ features/steps/registration_steps.py
@@ -0,0 +1,10 @@
+from behave.runner import Context
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+
